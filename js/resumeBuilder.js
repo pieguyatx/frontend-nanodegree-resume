4dd321e13937b1ac2a76bfe28552d3acb38735a7
Pius Wong <pius@pioslabs.com>
2016-09-29 18:18:24 -0500

    Restructure array to add more contacts

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,10 +6,15 @@ This has JavaScript to build the resume.
 var bio = {
   "name": "Jonesy Jackrabbit",
   "role": "Master of the Universe",
-  "contactInfo": "jonesy@mastersoftheuniverse.com",
+  "contacts": {
+    "mobile": "999-999-9999",
+    "email": "jonesy@mastersoftheuniverse.com",
+    "twitter": "@PBNJSammiches",
+    "location": "Mt. Etna, Spain"
+  },
   "pictureURL": "images/fry.jpg",
   "welcomeMessage": "Howdy, folks! Welcome this swanky pad!"
-}
+};
 
 // Add name and title
 var formattedName = HTMLheaderName.replace("%data%",bio.name);
@@ -18,8 +23,10 @@ $("#header").prepend([formattedRole]);
 $("#header").prepend([formattedName]);
 
 // Add other bio info
-var formattedContact = HTMLemail.replace("%data%",bio.contactInfo);
-$("#header").append([formattedContact]);
+var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+$("#header").append([formattedEmail]);
+var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
+$("#header").append([formattedLocation]);
 var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 $("#header").append([formattedWelcome]);
 
