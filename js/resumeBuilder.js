63c0d7e22beea1fdfb6e256a30a44c0c88c608f9
Pius Wong <pius@pioslabs.com>
2016-09-29 18:12:48 -0500

    Add array "object" with bio info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,8 +2,27 @@
 This has JavaScript to build the resume.
  */
 
+ //Create object: bio
+var bio = {
+  "name": "Jonesy Jackrabbit",
+  "role": "Master of the Universe",
+  "contactInfo": "jonesy@mastersoftheuniverse.com",
+  "pictureURL": "images/fry.jpg",
+  "welcomeMessage": "Howdy, folks! Welcome this swanky pad!"
+}
+
 // Add name and title
-var formattedName = HTMLheaderName.replace("%data%","Jonesy Jackrabbit");
-var formattedRole = HTMLheaderRole.replace("%data%","Master of the Universe");
+var formattedName = HTMLheaderName.replace("%data%",bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
 $("#header").prepend([formattedRole]);
 $("#header").prepend([formattedName]);
+
+// Add other bio info
+var formattedContact = HTMLemail.replace("%data%",bio.contactInfo);
+$("#header").append([formattedContact]);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+$("#header").append([formattedWelcome]);
+
+// Add photo
+var formattedBiopic = HTMLbioPic.replace("%data%",bio.pictureURL);
+$("#header").append([formattedBiopic]);
