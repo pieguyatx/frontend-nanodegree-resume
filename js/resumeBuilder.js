bb9af3c9a383260241d017a2c64de159c1377084
Pius Wong <pius@pioslabs.com>
2016-09-29 15:58:58 -0500

    Test vars, console, append, replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,4 +3,11 @@ This has JavaScript to build the resume.
  */
 
 // Script to add name
- $("#main").append([Pius Wong]);
+ $("#main").append(["Pius Wong"]);
+
+// Testing variables
+var awesomeThoughts = "I am Pius, and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+console.log("awesomeThoughts: ",awesomeThoughts);
+console.log("funThoughts: ", funThoughts);
+ $("#main").append(["<hr>",funThoughts]);
